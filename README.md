86eb2047297943f07b5c5255104dd7bee3f6e134
Vinit Kanani <vinitpankaj.kanani@sjsu.edu>
2022-11-02 01:01:11 -0700

    added data and readme

---
+++ README.md
@@ -0,0 +1,20 @@
+# Project - Sentiment Analysis - CMPE 257
+
+## Team Members
+
+- [Prabhath Reddy Gujavarthy]()
+- [Reshma Chowdary Bobba]()
+- [Sathvick Reddy Narahari]()
+- [Vinit Kanani](https://www.github.com/iamvinitk)
+
+## 1. Dataset
+### 1.1. Data Source
+[Sentiment140](http://help.sentiment140.com/for-students/)
+
+### 1.2. Data Description
+
+## 2. Problem Statement
+
+## 3. Methodology
+
+## 4. Results
--- main.py
+++ main.py
@@ -1,16 +1,56 @@
-# This is a sample Python script.
+"""
+0 - the polarity of the tweet (0 = negative, 2 = neutral, 4 = positive)
+1 - the id of the tweet (2087)
+2 - the date of the tweet (Sat May 16 23:58:44 UTC 2009)
+3 - the query (lyx). If there is no query, then this value is NO_QUERY.
+4 - the user that tweeted (robotickilldozr)
+5 - the text of the tweet (Lyx is cool)
+"""
 
-# Press ⌃R to execute it or replace it with your code.
-# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
+# read the data using pandas
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+import contractions
 
+# read the data
+data = pd.read_csv('data/training.csv', encoding="ISO-8859-1", header=None)
+data.columns = ['sentiment', 'id', 'date', 'query', 'user', 'text']
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
+# check the data
+print(data.head())
 
+# check the shape of the data
+print(data.shape)
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+# check the distribution of the data
+print(data.groupby('sentiment').size())
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+# check the distribution of the data
+sns.countplot(data['sentiment'], label='Count', hue_order=[0, 4])
+plt.show()
+
+# check the number of missing values
+print("Missing Values", data.isnull().sum())
+
+# count the text field for http links
+print("Number of http links", data['text'].str.count('http').sum())
+
+# remove the http links
+data['text'] = data['text'].str.replace('http\S+|www.\S+', '', case=False)
+
+# count the text field for @ mentions
+print("Number of @ mentions", data['text'].str.count('@').sum())
+
+# remove the @ mentions
+data['text'] = data['text'].str.replace('@\S+', '', case=False)
+
+# print the first 5 rows of the data
+print(data.head())
+
+# convert all the short forms to their full forms
+
+data['text'] = data['text'].apply(lambda x: [contractions.fix(word) for word in x.split()])
+
+# print the first 5 rows of the data
+print(data.head())
